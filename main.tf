61a8b427030abedbe2f812a0fe3f5100f0a94ab5
VinodDevOpsTech <vinodkumar160107@gmail.com>
2026-07-05 22:14:40 +0530

    hello

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "main" {
-    name        = local.common_name
+    name        = "${var.project}-${var.environment}-${var.sg_name}"
     description = "Allow TLS inbound traffic and all outbound traffic"
     vpc_id      = local.vpc_id
 
